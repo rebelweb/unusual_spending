39c4a66efda20b56fa1005824bb6362e47f9609d
Ryan Condron <rebelwebdevelopment@gmail.com>
2022-03-05 00:18:02 -0600

    added alert api endpoint

---
+++ app/api/v1/alerts.rb
@@ -0,0 +1,21 @@
+require 'grape'
+
+module API
+  module V1
+    class Alerts < Grape::API
+      version 'v1', using: :header
+      format :json
+      prefix :api
+
+      resources :alerts do
+        desc 'Sends Unusual Spending Alert'
+        params do
+          requires :user_id, type: Integer, desc: 'User Id to send alert for'
+        end
+        post do
+          UnusualSpendingAlertOperaation.new.call(params)
+        end
+      end
+    end
+  end
+end
--- spec/e2e/alerts_e2e_spec.rb
+++ spec/e2e/alerts_e2e_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Alerts End To End', type: :e2e do
   it 'sends alerts' do
     expect(UnusualExpensesAlertOperation).to receive(:call)
 
-    post '/alerts'
+    post '/api/alerts', params: { user_id: 1 }
 
     expect(response.status).to eq(200)
   end
